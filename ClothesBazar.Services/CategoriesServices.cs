9c35ef7862dd707450e815e9f032dd09db38ab7d
kumarvashist47863 <kumarvashist47863@gmail.com>
2021-07-03 17:37:25 +0530

    CRUD operation for Cateogry

---
+++ ClothesBazar.Services/CategoriesServices.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClothesBazar.Entities;
+using ClothesBazar.Database;
+
+namespace ClothesBazar.Services
+{
+    public class CategoriesServices
+    {
+
+        /// <summary>
+        /// SK: Get particualr category from DB
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public Category GetCategory(int ID)
+        {
+            using (var context= new CBContext())
+            {
+                return context.Categories.Find(ID);
+            }
+        }
+
+        /// <summary>
+        /// SK: This method is used to get all categories from DB
+        /// </summary>
+        /// <returns></returns>
+        public List<Category> GetCategories()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        /// <summary>
+        /// SK: Save Category record in database
+        /// </summary>
+        /// <param name="category"></param>
+        public void SaveCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// SK: Update Category in database
+        /// </summary>
+        /// <param name="category"></param>
+        public void UpdateCategory(Category category)
+        {
+            using (var context=new CBContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// SK: delete Category in database
+        /// </summary>
+        /// <param name="category"></param>
+        public void DeleteCategory(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                var category = context.Categories.Find(ID);
+                context.Categories.Remove(category);
+                context.SaveChanges();
+
+            }
+        }
+
+
+    }
+}
---
+++ ClothesBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClothesBazar.Entities;
+using ClothesBazar.Services;
+namespace ClothesBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesServices categoryService = new CategoriesServices();
+
+        /// <summary>
+        /// SK: Used to call category service to get all category data from DB
+        /// Display all category
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetCategories();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// SK: Used to call category service to get partiuclar category from Db 
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+
+        /// <summary>
+        /// SK: Used to call category service to update partiuclar category from Db 
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryService.UpdateCategory(category);
+           // return View();
+           return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            categoryService.DeleteCategory(category.ID);
+            return RedirectToAction("Index");
+        }
+
+
+
+        /// <summary>
+        /// Sk: This method is calling Category Service class to save data into DB
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return View();
+        }
+    }
+}
