8dd50a649617d76305d72094ea5d16e9c28cd9ce
kumarvashist47863 <kumarvashist47863@gmail.com>
2021-07-04 12:02:56 +0530

    Create, save , show and search product.

---
+++ ClothesBazar.Services/ProductsService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClothesBazar.Entities;
+using ClothesBazar.Database;
+
+
+namespace ClothesBazar.Services
+{
+   public  class ProductsService
+    {
+        /// <summary>
+        /// SK: Get particualr category from DB
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public Product GetProduct(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        /// <summary>
+        /// SK: This method is used to get all Product from DB
+        /// </summary>
+        /// <returns></returns>
+        public List<Product> GetProduct()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        /// <summary>
+        /// SK: Save Product record in database
+        /// </summary>
+        /// <param name="product"></param>
+        public void SaveProduct(Product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// SK: Update Product in database
+        /// </summary>
+        /// <param name="Products"></param>
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// SK: delete product in database
+        /// </summary>
+        /// <param name="product"></param>
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                var product = context.Products.Find(ID);
+                context.Products.Remove(product);
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ ClothesBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClothesBazar.Entities;
+using ClothesBazar.Services;
+namespace ClothesBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// SK: Display all products
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ProudctTable(string search)
+        {
+
+            var products = productsService.GetProduct();
+            //products.Where(p => p.Name == search).ToList();
+            if (string.IsNullOrEmpty(search) == false)
+            {
+                products = products.Where(p => p.Name!=null && p.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+                //return View(products);
+            return PartialView(products);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+
+        /// <summary>
+        /// Sk: This method is calling Product Service class to save product data into DB
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProudctTable");
+        }
+
+
+    }
+}
